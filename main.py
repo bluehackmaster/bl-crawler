420d27d257b9c679c7c2d14ec7c12f19feea3b14
Youngbok Yoon <master@bluehack.net>
2017-11-14 21:46:12 +0900

    [BL-119] Sample use-case using stylelens-crawl

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
-import subprocess
+from __future__ import print_function
 
-from util.bigquery import *
+import os
+import redis
+from stylelens_product import Product
+from stylelens_product import ProductApi
+from stylelens_product.rest import ApiException
+from stylelens_crawl import Crawler
+
+REDIS_SERVER = os.environ['REDIS_SERVER']
+REDIS_PASSWORD = os.environ['REDIS_PASSWORD']
+PRODUCT_VERSION = os.environ['PRODUCT_VERSION']
+HOST_CODE = os.environ['HOST_CODE']
+
+rconn = redis.StrictRedis(REDIS_SERVER, port=6379, password=REDIS_PASSWORD)
 
 if __name__ == '__main__':
-    service_name = os.environ.get('service', 'imvely')
 
-    if os.path.exists('out.json'):
-        os.remove('out.json')
+  options = {}
+  options['host_code'] = HOST_CODE
 
-    print(service_name)
-    if service_name == '8seconds':
-        subprocess.run("scrapy runspider service/8seconds.py -o out.json -t json", stderr=subprocess.PIPE, shell=True)
-    elif service_name == 'naning9':
-        subprocess.run("scrapy runspider service/naning9.py -o out.json -t json", stderr=subprocess.PIPE, shell=True)
-    elif service_name == 'imvely':
-        subprocess.run("scrapy runspider service/imvely.py -o out.json -t json", stderr=subprocess.PIPE, shell=True)
-    elif service_name == 'stylenanda':
-        subprocess.run("scrapy runspider service/stylenanda.py -o out.json -t json", stderr=subprocess.PIPE, shell=True)
 
-    if os.path.exists('out.json'):
-        bigquery = BigQuery(service_name)
-        bigquery.upload_data_from_json()
+  rconn.lpush(HOST_CODE, 'started')
+
+  crawler = Crawler(options)
+
+  items = crawler.run()
+
+  api_instance = ProductApi()
+  product = Product()
+  for item in items:
+    product.host_url = item['host_url']
+    product.host_code = item['host_code']
+    product.version = PRODUCT_VERSION
+    # ...
+
+    try:
+      # Added a new Product
+      api_response = api_instance.add_product(product)
+    except ApiException as e:
+      print("Exception when calling ProductApi->add_product: %s\n" % e)
+
+  rconn.lpop(HOST_CODE)
+
---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export ORG="bluelens"
+export IMAGE="bl-cropper"
+export TAG='latest'
+export NAMESPACE="index"
+
+docker login
+
+docker build -t $IMAGE:$TAG .
+docker tag $IMAGE:$TAG $ORG/$IMAGE:$TAG
+docker push $ORG/$IMAGE:$TAG
