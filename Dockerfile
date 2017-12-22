95ca9d5886854a6213f6ce09bdcdf78abbc411de
Youngbok Yoon <master@bluehack.net>
2017-12-22 22:08:34 +0900

    Updated stylelens-product

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,12 @@
 FROM bluelens/ubuntu-16.04:py3
 MAINTAINER bluehackmaster <master@bluehack.net>
 
+RUN mkdir -p /tmp
 RUN mkdir -p /usr/src/app
 
 WORKDIR /usr/src/app
 
+COPY gdoc_certi.json /tmp
 COPY requirements.txt /usr/src/app/
 RUN pip install --no-cache-dir -r requirements.txt
 
--- main.py
+++ main.py
@@ -2,18 +2,14 @@ from __future__ import print_function
 
 import redis
 import os
-from multiprocessing import Process
-from threading import Timer
+# from multiprocessing import Process
+# from threading import Timer
 from bluelens_spawning_pool import spawning_pool
 from stylelens_crawl.stylens_crawl import StylensCrawler
 from bluelens_log import Logging
-from stylelens_product import Product
-from stylelens_product import ProductApi
-from stylelens_product.models.add_product_response import AddProductResponse
-from stylelens_product.rest import ApiException
+from stylelens_product.products import Products
 
-
-HEALTH_CHECK_TIME = 60 * 60 * 24
+# HEALTH_CHECK_TIME = 60 * 60 * 24
 
 REDIS_HOST_CLASSIFY_QUEUE = 'bl:host:classify:queue'
 REDIS_HOST_CRAWL_QUEUE = 'bl:host:crawl:queue'
@@ -23,6 +19,7 @@ REDIS_CRAWL_VERSION_LATEST = 'latest'
 
 
 SPAWN_ID = os.environ['SPAWN_ID']
+HOST_CODE = os.environ['HOST_CODE']
 RELEASE_MODE = os.environ['RELEASE_MODE']
 REDIS_SERVER = os.environ['REDIS_SERVER']
 REDIS_PASSWORD = os.environ['REDIS_PASSWORD']
@@ -34,6 +31,8 @@ options = {
   'REDIS_PASSWORD': REDIS_PASSWORD
 }
 log = Logging(options, tag='bl-crawler')
+
+product_api = None
 
 # heart_bit = True
 
@@ -63,8 +62,10 @@ def get_latest_crawl_version():
   return version_id
 
 def crawl(host_code, version_id):
+  global product_api
+  product_api = Products()
   options = {}
-  log.setTag('bl-crawler-' + SPAWN_ID + ':' + host_code)
+  log.setTag('bl-crawler-' + SPAWN_ID)
   log.debug('start crawl')
   options['host_code'] = host_code
 
@@ -74,44 +75,41 @@ def crawl(host_code, version_id):
     if crawler.start() == True:
       items = crawler.get_items()
 
-      product_api = ProductApi()
       for item in items:
-        product = Product()
-        product.name = item['name']
-        product.host_url = item['host_url']
-        product.host_code = item['host_code']
-        product.host_name = item['host_name']
-        product.product_no = item['product_no']
-        product.main_image = item['main_image']
-        product.sub_images = item['sub_images']
+        product = {}
+        product['name'] = item['name']
+        product['host_url'] = item['host_url']
+        product['host_code'] = item['host_code']
+        product['host_name'] = item['host_name']
+        product['product_no'] = item['product_no']
+        product['main_image'] = item['main_image']
+        product['sub_images'] = item['sub_images']
 
         try:
-          res = product_api.update_product_by_hostcode_and_productno(host_code=host_code,
-                                                                     product_no=product.product_no,
-                                                                     body=product)
-          product.version_id = version_id
-          product.product_url = item['product_url']
-          product.tags = item['tags']
-          product.price = item['price']
-          product.currency_unit = item['currency_unit']
-          product.nation = item['nation']
+          res = product_api.update_product_by_hostcode_and_productno(product)
+          product['version_id'] = version_id
+          product['product_url'] = item['product_url']
+          product['tags'] = item['tags']
+          product['price'] = item['price']
+          product['currency_unit'] = item['currency_unit']
+          product['nation'] = item['nation']
 
-          if res.created_count > 0:
-            log.debug("Created a product: " + res.product_id)
-            product.id = res.product_id
-            product.is_indexed = False
-            update_product_by_id(product)
-          elif res.modified_count > 0:
-            log.debug("Existing product is updated: product_no:" + product.product_no)
-            product.is_indexed = False
+          if 'upserted' in res:
+            product_id = str(res['upserted'])
+            log.debug("Created a product: " + product_id)
+            product['is_processed'] = False
+            update_product_by_id(product_id, product)
+          elif res['nModified'] > 0:
+            log.debug("Existing product is updated: product_no:" + product['product_no'])
+            product['is_processed']= False
             update_product_by_hostcode_and_productno(product)
           else:
             log.debug("The product is same")
-            # product.is_indexed = True
+            product['is_processed']= True
             update_product_by_hostcode_and_productno(product)
-        except ApiException as e:
+        except Exception as e:
           log.error("Exception when calling ProductApi->update_product_by_hostcode_and_productno: %s\n" % e)
-          delete_pod()
+          # delete_pod()
 
   except Exception as e:
     log.error("host_code:" + host_code + 'error: ' + str(e))
@@ -120,46 +118,45 @@ def crawl(host_code, version_id):
   notify_to_classify(host_code)
   delete_pod()
 
-def update_product_by_id(product):
-  log.debug('update_product_by_id:' + product.product_no)
-  product_api = ProductApi()
+def update_product_by_id(id, product):
+  log.debug('update_product_by_id:' + product['product_no'])
+  global product_api
 
   try:
-    response = product_api.update_product_by_id(product.id, product)
+    response = product_api.update_product_by_id(id, product)
     # log.debug(response)
-  except ApiException as e:
+  except Exception as e:
     log.error("Exception when calling ProductApi->update_product_by_id: %s\n" % e)
-    delete_pod()
+    # delete_pod()
 
 def update_product_by_hostcode_and_productno(product):
-  log.debug('update_product_by_hostcode_and_productno:' + product.product_no)
-  product_api = ProductApi()
+  log.debug('update_product_by_hostcode_and_productno:' + product['product_no'])
+  global product_api
 
   try:
-    response = product_api.update_product_by_hostcode_and_productno(product.host_code, product.product_no, product)
+    response = product_api.update_product_by_hostcode_and_productno(product['host_code'], product['product_no'], product)
     # log.debug(response)
-  except ApiException as e:
+  except Exception as e:
     log.error("Exception when calling ProductApi->update_product_by_hostcode_and_productno: %s\n" % e)
-    delete_pod()
+    # delete_pod()
 
 def notify_to_classify(host_code):
   rconn.lpush(REDIS_HOST_CLASSIFY_QUEUE, host_code)
 
 def dispatch_job(rconn, version_id):
   log.info('Start dispatch_job')
+  # crawl('HC0001', "5a3bda9e4dfd7d90b88e5cde")
   # Timer(HEALTH_CHECK_TIME, check_health, ()).start()
   while True:
     key, value = rconn.blpop([REDIS_HOST_CRAWL_QUEUE])
-    crawl(value.decode('utf-8'), version_id)
     # global  heart_bit
     # heart_bit = True
 
-
 if __name__ == '__main__':
-  log.info('Start bl-crawler')
+  log.info('Start bl-crawler:new5')
   version_id = get_latest_crawl_version()
   try:
-    Process(target=dispatch_job, args=(rconn,version_id,)).start()
+    crawl(HOST_CODE, version_id)
   except Exception as e:
     log.error(str(e))
     delete_pod()
